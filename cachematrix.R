75ee55992dd02f98c8b7c27e292610ba27354d4a
AlchemyGeek <stathis@gmail.com>
2016-07-28 17:28:39 -0700

    Replaced template code with assignment solution.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## CacheMatrix caches the result of 
+## a matrix operation (inverse or other)
 
-## Write a short comment describing this function
-
+# Constructor for CacheMatrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Cached matrix value
+  mcached <- NULL
+  
+  ## Set the initial matrix value and initialize cache
+  set <- function(y) {
+    x <<- y
+    mcached <<- NULL
+  } 
+  
+  ## Return the matrix value
+  get <- function() x
+  
+  ## Set cache value
+  setcache <- function(calc) mcached <<- calc
+  getcache <- function() mcached
+  list(set = set, get = get,
+       setcache = setcache,
+       getcache = getcache)
 }
 
 
-## Write a short comment describing this function
-
+## Calculate Matrix Inverse operation
+## Return either cached value or new calculation
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  cache <- x$getcache()
+  if(!is.null(cache)) {
+    message("getting cached data")
+    return(cache)
+  }
+
+  ## Retrieve matrix
+  data <- x$get()
+  
+  ## Calculate Inverse
+  cache <- solve(data, ...)
+  
+  ## Set cache to inv matrix
+  x$setcache(cache)
+  
+  ## Return a matrix that is the inverse of 'x'
+  cache
 }
+
+
